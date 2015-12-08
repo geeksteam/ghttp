9f44b219e2fe10343e813f95a7b1478b52a4b91b
Dmitry Yakutkin <dmitry.yakutkin@gmail.com>
2015-12-08 10:40:50 +0200

    testing utemplates

--- utemplates/utemplates.go
+++ utemplates/utemplates.go
@@ -7,33 +7,29 @@ import (
 )
 
 var (
-	cfg                       Utemplates
-	BoltDBMain                string
-	BotlDBUserTemplatesBacket string
+	cfg Utemplates
 )
 
 func SetConfig(c Utemplates) {
 	cfg = c
-	BoltDBMain = cfg.BoltDBMain
-	BotlDBUserTemplatesBacket = cfg.BotlDBUserTemplatesBacket
 }
 
 // Set Creates new or update entry for template
 func Set(templateName string, templ UserTemplate) {
-	boltdb.DB(BoltDBMain, cfg.DataEncoding).Bucket(BotlDBUserTemplatesBacket).Set(templateName, templ)
+	boltdb.DB(cfg.BoltDBMain, cfg.DataEncoding).Bucket(cfg.BotlDBUserTemplatesBacket).Set(templateName, templ)
 }
 
 //Delete Deletes template from db
 func Delete(templateName string) {
-	boltdb.DB(BoltDBMain, cfg.DataEncoding).Bucket(BotlDBUserTemplatesBacket).Delete(templateName)
+	boltdb.DB(cfg.BoltDBMain, cfg.DataEncoding).Bucket(cfg.BotlDBUserTemplatesBacket).Delete(templateName)
 }
 
 // Get Returns template with gien name
 func Get(templateName string) *UserTemplate {
 	log.Printf("GETTING TEMPLATE %v\n", templateName)
 	templ := &UserTemplate{}
-	log.Printf("db main: %v, encoding: %v\n", BoltDBMain, cfg.DataEncoding)
-	err := boltdb.DB(BoltDBMain, cfg.DataEncoding).Bucket(BotlDBUserTemplatesBacket).Get(templateName, templ)
+	log.Printf("db main: %v, encoding: %v\n", cfg.BoltDBMain, cfg.DataEncoding)
+	err := boltdb.DB(cfg.BoltDBMain, cfg.DataEncoding).Bucket(cfg.BotlDBUserTemplatesBacket).Get(templateName, templ)
 	if err != nil {
 		log.Println("Can't load user tamplate:", err)
 		return nil
@@ -45,7 +41,7 @@ func Get(templateName string) *UserTemplate {
 func GetAll() (map[string]UserTemplate, error) {
 	templs := map[string]UserTemplate{}
 
-	ut, err := boltdb.DB(BoltDBMain, cfg.DataEncoding).Bucket(BotlDBUserTemplatesBacket).GetAll(&UserTemplate{})
+	ut, err := boltdb.DB(cfg.BoltDBMain, cfg.DataEncoding).Bucket(cfg.BotlDBUserTemplatesBacket).GetAll(&UserTemplate{})
 	if err != nil {
 		return templs, err
 	}
