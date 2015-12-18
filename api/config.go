89830a5b2b8c3cc1680f74f5f456b88b5a25e371
Dmitry Yakutkin <dmitry.yakutkin@gmail.com>
2015-12-18 15:39:03 +0200

    refactoring

--- api/config.go
+++
@@ -1,5 +0,0 @@
-package api
-
-type API struct {
-	ApiPath string `default:"./apicalls" comment:"Path to api folder"`
-}
--- bruteforce/types.go
+++
@@ -1,7 +0,0 @@
-package bruteforce
-
-// BruteIP is a info struct, which holds information about given IP's activity history.
-type BruteIP struct {
-	Timestamp int64 // Unixtime последней попытки
-	Attempts  int   // Количество попыток
-}
